13b1b63e4e98363bfad35c4e6f4acca031305367
Gregory Gerard <wako057@gmail.com>
2024-09-26 13:56:21 +0200

    feat: introduction + type inference

---
+++ basic.ts
@@ -0,0 +1,33 @@
+let age: number = 24;
+
+let userName: string = 'gregs';
+
+let isInstructor: boolean = false;
+
+/// More complex types
+
+let hobbies: string[] = ['Sports', 'Cooking'];
+
+let person1: object = { name: 'Greg', age: 44};
+let person2: any = { name: 'Greg', age: 44};
+
+let person3: {
+    name: string,
+    age: number,
+};
+
+person3 = { name: 'Greg', age: 44};
+
+let people: {
+    name: string,
+    age: number,
+}[];
+
+people = [{ name: 'Greg', age: 44}];
+
+// type inference
+
+let course1: string = 'React - The complete guide';
+let course2 = 'React - The complete guide';
+
+course2 = 123;
---
+++ with-typescript.ts
@@ -0,0 +1,7 @@
+function addTs(a: number, b: number) {
+    return a + b;
+  }
+  
+  const resultTs = addTs(2, 5);
+  
+  console.log(resultTs);
